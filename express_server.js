d2f96cf12423e798455a35b1f30b43bd657af94a
AVacAdok <avakuang8@gmail.com>
2022-06-15 16:26:00 +0000

    edit shortUrl to another longURL

--- express_server.js
+++ express_server.js
@@ -80,19 +80,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-//---------------------------need to fix this part--------------------------------
-app.get("url/:shortURL/edit", (req, res) => {
-  let shortURL = req.params.shortURL;
-  console.log(shortURL);
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect("/urls");
-});
-
 app.post("/urls/:shortURL/edit", (req, res) => {
   let shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect("/urls");
+  res.redirect("/urls/");
 });
+
+app.get("/urls/:shortURL/edit", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
+  res.render("urls_show", templateVars);
+})
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
